7f1622ce64d5289f1cff0cbe757b45ec59fb3837
Dale Zak <dalezak@gmail.com>
2025-04-04 11:06:04 -0600

    Added loading spinner to turbo-frame

--- app/javascript/controllers/geocode_controller.js
+++ app/javascript/controllers/geocode_controller.js
@@ -2,29 +2,34 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
 
-  static targets = ["form", "latitude", "longitude", "address", "error"];
+  static targets = ["form", "latitude", "longitude", "error", "info"];
 
   locate(event) {
     event.preventDefault();
-    this.errorTarget.textContent = "";
     if (navigator.geolocation) {
+      this.infoTarget.textContent = "Detecting your location...";
+      this.errorTarget.textContent = "";
       navigator.geolocation.getCurrentPosition(
         this.handleSuccess.bind(this),
         this.handleError.bind(this)
       )
     } 
     else {
+      this.infoTarget.textContent = "";
       this.errorTarget.textContent = "Geolocation is not supported by this browser.";
     }
   }
   
   handleSuccess(position) {
+    this.infoTarget.textContent = "";
+    this.errorTarget.textContent = "";
     this.latitudeTarget.value = position.coords.latitude;
     this.longitudeTarget.value = position.coords.longitude;
     this.formTarget.requestSubmit();
   }
 
   handleError(error) {
+    this.infoTarget.textContent = "";
     this.errorTarget.textContent = `There was a problem detecting your location: ${error.message}`;
   }
 
