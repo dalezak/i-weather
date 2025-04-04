ac66cc977a5d7d807b0defd0ea4c251721b9d61d
Dale Zak <dalezak@gmail.com>
2025-04-04 15:09:35 -0600

    Updated about page and readme

--- README.md
+++ README.md
@@ -8,17 +8,19 @@
 - **Rails** - Built using the latest version of Rails.
 - **Hotwire** - Forecast displayed using Hotwire's turbo-frames.
 - **Stimulus** - Geocoder location detection using Stimulus.
-- **Bootstrap Design** - Responsive design using Bootstrap.
+- **Bootstrap** - Responsive design using Bootstrap.
 - **Geolocation** - Address lookup using Geocoder.
-- **Weather API** - Weather data from Weather API.
+- **APIs** - Weather data from Weather API.
 - **Measurements** - Supports both metric and imperial units.
 - **Cookies** - Cookies for storing user preferences.
+- **Caching** - Forecast caching for performance.
 - **Service Objects** - Service objects for business logic.
 - **View Partials** - Reusable partials for views.
 - **View Helpers** - Resuable view helpers for common tasks.
 - **Credentials** - Rails credentials for sensitive data.
 - **Routes** - Routes defined using resources convention.
 - **Tests** - Controller, model, and system tests in Minitest.
+- **Code Comments** - Code is well-commented for clarity.
 - **Source Code** - Source code available on GitHub.
 - **MIT License** - Open source under the MIT license.
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,21 +3,28 @@ class PagesController < ApplicationController
   # It provides methods to render the home page and the about page.
   # @note The home page displays the weather forecast, while the about page provides information about the application.
   def about
+    @description = [
+      "iWeather is a simple weather application that provides current weather information and forecasts for any location.",
+      "It is built using Ruby on Rails and utilizes various modern web technologies to deliver a seamless user experience."
+    ]
+    @website = "https://github.com/dalezak/i-weather"
     @features = {
       "Rails" => "Built using the latest version of Rails.",
       "Hotwire" => "Forecast displayed using Hotwire's turbo-frames.",
       "Stimulus" => "Geocoder location detection using Stimulus.",
-      "Bootstrap Design" => "Responsive design using Bootstrap.",
+      "Bootstrap" => "Responsive design using Bootstrap.",
       "Geolocation" => "Address lookup using Geocoder.",
-      "Weather API" => "Weather data from Weather API.",
+      "APIs" => "Weather data from Weather API.",
       "Measurements" => "Supports both metric and imperial units.",
       "Cookies" => "Cookies for storing user preferences.",
+      "Caching" => "Forecast caching for performance.",
       "Service Objects" => "Service objects for business logic.",
       "View Partials" => "Reusable partials for views.",
       "View Helpers" => "Resuable view helpers for common tasks.",
       "Credentials" => "Rails credentials for sensitive data.",
       "Routes" => "Routes defined using resources convention.",
       "Tests" => "Controller, model, and system tests in Minitest.",
+      "Code Comments" => "Code is well-commented for clarity.",
       "Source Code" => "Source code available on GitHub.",
       "MIT License" => "Open source under the MIT license."
     }
