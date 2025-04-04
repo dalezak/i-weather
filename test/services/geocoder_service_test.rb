4332002f244294ff4a75bdf7c802f089b155c3e0
Dale Zak <dalezak@gmail.com>
2025-04-04 13:45:51 -0600

    Updated service tests

--- test/services/geocoder_service_test.rb
+++ test/services/geocoder_service_test.rb
@@ -2,20 +2,18 @@ require 'test_helper'
 
 class GeocoderServiceTest < ActiveSupport::TestCase
   test 'no latitude or longitude returns nil' do
-    service = GeocoderService.new(nil, nil)
-    assert_nil service.call
+    results = GeocoderService.call(nil, nil)
+    assert_nil results
   end
 
   test 'invalid latitude and longitude returns nil' do
-    service = GeocoderService.new(999, 999)
-    result = service.call
-    assert_nil result
+    results = GeocoderService.call(999, 999)
+    assert_nil results
   end
 
   test 'valid latitude and longitude returns location' do
-    service = GeocoderService.new(52.146973, -106.647034)
-    result = service.call
-    assert_not_nil result
-    assert_match(/Saskatoon, Saskatchewan/, result)
+    results = GeocoderService.call(52.146973, -106.647034)
+    assert_not_nil results
+    assert_match(/Saskatoon, Saskatchewan/, results)
   end
 end
--- test/services/weather_api_service_test.rb
+++ test/services/weather_api_service_test.rb
@@ -2,23 +2,41 @@ require 'test_helper'
 
 class WeatherApiServiceTest < ActiveSupport::TestCase
   test 'no query returns nil' do
-    service = WeatherApiService.new(nil, nil)
-    assert_nil service.call
+    results = WeatherApiService.call(nil, nil)
+    assert_nil results
   end
 
   test 'no units returns nil' do
-    service = WeatherApiService.new(nil, nil)
-    assert_nil service.call
+    results = WeatherApiService.call("Saskatoon", nil)
+    assert_nil results
   end
 
   test 'invalid units returns nil' do
-    service = WeatherApiService.new("Saskatoon", "abc")
-    assert_nil service.call
+    results = WeatherApiService.call("Saskatoon", "abc")
+    assert_nil results
   end
 
-  test 'valid latitude and longitude returns location' do
-    service = WeatherApiService.new("Saskatoon", "metric")
-    result = service.call
-    assert_not_nil result
+  test 'valid location returns forecast' do
+    mock_temperature = "20°C"
+    mock_forecast = Forecast.new(temperature: mock_temperature, updated_at: Time.now)
+    mock = Minitest::Mock.new
+    mock.expect(:call, mock_forecast, ["Saskatoon", "metric"])
+    WeatherApiService.stub :call, mock do
+      results = WeatherApiService.call("Saskatoon", "metric")
+      assert_not_nil results
+      assert_equal results.valid?, true
+      assert_equal mock_temperature, results.temperature
+    end
+  end
+
+  test 'invalid location returns forecast with valid? false' do
+    mock_forecast = Forecast.new(updated_at: nil)
+    mock = Minitest::Mock.new
+    mock.expect(:call, mock_forecast, ["Saskatoon", "metric"])
+    WeatherApiService.stub :call, mock do
+      results = WeatherApiService.call("Saskatoon", "metric")
+      assert_not_nil results
+      assert_equal results.valid?, false
+    end
   end
 end
