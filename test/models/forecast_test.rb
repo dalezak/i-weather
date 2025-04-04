80e322ecdaa5a7606aeb74138efdac702fc719ea
Dale Zak <dalezak@gmail.com>
2025-04-04 13:47:47 -0600

    Updated forecast_test

--- test/models/forecast_test.rb
+++ test/models/forecast_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ForecastTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  test "valid? is false" do
+    forecast = Forecast.new
+    assert_not forecast.valid?
+  end
+
+  test 'valid? is true' do
+    forecast = Forecast.new(updated_at: Time.now)
+    assert forecast.valid?
   end
 end
