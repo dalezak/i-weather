22f7970372f1cc466d9e232af01fa83c3c8c0257
Dale Zak <dalezak@gmail.com>
2025-04-07 11:19:09 -0600

    Added comments to application_service and application_parser

--- app/parsers/application_parser.rb
+++ app/parsers/application_parser.rb
@@ -1,4 +1,12 @@
+# This is a base class for parsers that can be used to parse various types of data.
 class ApplicationParser
+  # The `parse` method is the main entry point for the parser.
+  # It should be overridden in subclasses to implement the desired parsing behavior.
+  def parse
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+
+  # This method allows you to call the parser directly without needing to create an instance first.
   def self.parse(*args, &block)
     new(*args, &block).parse
   end
--- app/services/application_service.rb
+++ app/services/application_service.rb
@@ -1,4 +1,12 @@
+# This is a base class for all service objects in the application.
 class ApplicationService
+  # The `call` method is the main entry point for the service object.
+  # It should be overridden in subclasses to implement the desired behavior.
+  def call
+    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
+  end
+
+  # This method allows you to call the service object directly without needing to create an instance first.
   def self.call(*args, &block)
     new(*args, &block).call
   end
