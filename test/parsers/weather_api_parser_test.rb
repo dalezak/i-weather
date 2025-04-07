3fb6926f59dba118a25e91affc6ca1910e963245
Dale Zak <dalezak@gmail.com>
2025-04-06 21:35:57 -0600

    Added some tests to weather_api_parser_test

--- test/parsers/weather_api_parser_test.rb
+++ test/parsers/weather_api_parser_test.rb
@@ -25,4 +25,14 @@ class WeatherApiParserTest < ActiveSupport::TestCase
     assert_equal forecast.updated_at, updated_at
     assert_equal forecast.valid?, true
   end
+
+  test "has correct symbol for metric units" do
+    forecast = WeatherApiParser.parse({ current: { last_updated: Time.now.to_s, temp_c: 20 } }, "metric")
+    assert_equal "20°C", forecast.temperature
+  end
+
+  test "has correct symbol for imperial units" do
+    forecast = WeatherApiParser.parse({ current: { last_updated: Time.now.to_s, temp_f: 68 } }, "imperial")
+    assert_equal "68°F", forecast.temperature
+  end
 end
